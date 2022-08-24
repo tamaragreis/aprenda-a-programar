8c0b26b2d617ccb780555585b47fc4f93aab4c10
Tamara Reis <tamarinhajb@gmail.com>
2022-08-24 17:08:14 -0300

    Adicionei o desafio

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,4 +8,8 @@ console.log(nome)
 console.log(email)
 console.log(profissão)
 console.log(idade)
-console.log(Lugar)
+console.log (Lugar)
+
+
+
+console.log( `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este e-mail, pois acabamos de confirmar as compras das passagens aéreas para ${Lugar}. Confirma pra gente alguns dados? A sua idade é ${idade} você é ${profissão} e o seu e-mail é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
