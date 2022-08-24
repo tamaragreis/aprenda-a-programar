4e5af3387a8ce3eb688861dcd01923f6d438c566
Tamara Reis <tamarinhajb@gmail.com>
2022-08-24 16:45:46 -0300

    agencia de viagens

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ email = "tamarinhajb@gmail.com"
 profissão = "Cientista de Alimentos"
 idade = "29 anos"
 Lugar = "Praga"
+
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(Lugar)
