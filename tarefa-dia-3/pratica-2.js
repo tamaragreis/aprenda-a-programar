b9562c839f9319845f0134d8ef229f815c18410c
Tamara Reis <tamarinhajb@gmail.com>
2022-08-29 08:45:23 -0300

    fim da tarefa

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 10
+let n = 10 
+let i = 1
+
+while (i <= n) { 
+let resultado = multiplicador * i 
+let textoDeExibicao = `${multiplicador} X ${i} = ${resultado}`
+    console.log(textoDeExibicao)
+    i++
+}
---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,15 @@
+let num1 = 1
+let num2 = 16
+let cont = 1
+
+while (num1 <= num2) 
+{
+    if (num1 % 2 == 1) 
+    {
+        console.log(num1)
+    }
+    num1++
+}
+
+
+
